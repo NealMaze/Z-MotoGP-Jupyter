f306a71630446f2f0a1c3e2a07a533cee847d64c
Neal Maze <nealwmaze@gmail.com>
2021-08-22 16:16:13 -0600

    still working

--- lists.py
+++ lists.py
@@ -3,7 +3,6 @@ import tabula
 from tabula.io import read_pdf
 import fnmatch
 from os import listdir, mkdir
-from os import listdir, mkdir
 import pdfplumber as plumb
 import fnmatch
 import pandas as pd
